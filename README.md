c3c72ae3af610011ae18713d29006c49572fdbd7
Justin Young <jyoung@sonatype.com>
2020-09-04 16:42:13 -0700

    Let there be bikes.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Viva La Bicicleta
+
+A script to pull activity information from Strava and post to a Slack thread.
+
+## Technical Notes
+
+### history.json
+
+Due to the fact the Stava API does not provide timestamp, a history.json file is kept locally
+with approximated buckets of when the events occurred (derived from when the script is run).
+
+### auth.json
+
+An auth.json file is required with the following format
+
+```
+{
+	"strava": {
+		"client_id": ,
+		"client_secret": "",
+		"access_token": "",
+		"refresh_token": ""
+	},
+	"slack": {
+		"token": ""
+	}
+}
+```
---
+++ index.js
@@ -0,0 +1,305 @@
+const auth = require('./auth.json');
+
+const open = require('open')
+const strava = require('strava-v3');
+const slack = require('slack')
+
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util')
+
+const listActivities = promisify(strava.clubs.listActivities).bind(strava.clubs);
+
+function setup() {
+  return new Promise((resolve, reject) => {
+    const historyFileName = path.resolve(__dirname, 'history.json');
+    fs.open(historyFileName, (err, file) => {
+      if (err) {
+        fs.writeFile(historyFileName, '{}', (err) => {
+          if (err) {
+            reject(err);
+          }
+          resolve();
+        });
+      }
+      resolve();
+    })
+  });
+}
+
+function configStrava() {
+  strava.config({
+    'access_token': auth.strava.access_token,
+    'client_id': auth.strava.client_id,
+    'client_secret': auth.strava.client_secret
+  });
+}
+
+async function getNewStravaToken() {
+  return new Promise((resolve, reject) => {
+    configStrava();
+    const readline = require('readline');
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    console.log('Navigate to:');
+    console.log(`http://www.strava.com/oauth/authorize?client_id=${auth.strava.client_id}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=auto&scope=read,activity:read`)
+    rl.question('Please enter the code:', code => {
+      rl.close();
+      strava.oauth.getToken(code, (err, payload) => {
+        auth.strava.access_token = payload.body.access_token;
+        auth.strava.refresh_token = payload.body.refresh_token;
+        fs.writeFileSync(path.resolve(__dirname, 'auth.json'), JSON.stringify(auth, null, '\t'));
+        resolve();
+      });
+    });
+  });
+}
+
+async function refreshStravaToken() {
+  if (!auth.strava.access_token || !auth.strava.refresh_token) {
+    await getNewStravaToken();
+    configStrava();
+  }
+  configStrava();
+  try {
+    const token = await strava.oauth.refreshToken(auth.strava.refresh_token);
+    auth.strava.access_token = token.access_token;
+    auth.strava.refresh_token = token.refresh_token;
+    fs.writeFileSync(path.resolve(__dirname, 'auth.json'), JSON.stringify(auth, null, '\t'));
+    configStrava();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function doWork() {
+  try {
+    const activities = await listActivities({id: 322307, per_page: 100, page: 0, "access_token": auth.strava.access_token});
+    const historyJson= fs.readFileSync(path.resolve(__dirname, 'history.json'));
+    const history = JSON.parse(historyJson);
+
+    const newActivities = activities.filter(activity => {
+      return !Object.keys(history).some(day => {
+        return history[day].some(record => {
+          return activity.athlete.firstname == record.firstname &&
+            activity.athlete.lastname == record.lastname &&
+            activity.distance == record.distance &&
+            activity.elapsed_time == record.elapsed_time &&
+            activity.moving_time == record.moving_time &&
+            activity.name == record.name &&
+            activity.total_elevation_gain == record.total_elevation_gain &&
+            activity.type == record.type &&
+            activity.workout_type == record.workout_type;
+        });
+      });
+    });
+
+    const todayDate = new Date();
+    const today = todayDate.toISOString().split('T')[0];
+    if (!history[today]) {
+      history[today] = [];
+    }
+
+    newActivities.forEach(activity => {
+      history[today].push({
+        firstname: activity.athlete.firstname,
+        lastname: activity.athlete.lastname,
+        distance: activity.distance,
+        elapsed_time: activity.elapsed_time,
+        moving_time: activity.moving_time,
+        name: activity.name,
+        total_elevation_gain: activity.total_elevation_gain,
+        type: activity.type,
+        workout_type: activity.workout_type
+      });
+    });
+
+    fs.writeFileSync(path.resolve(__dirname, 'history.json'), JSON.stringify(history, null, '\t'));
+
+    await reportProgress(history, newActivities)
+  } catch (err) {
+    if (err.statusCode == 401) {
+      await refreshStravaToken();
+      await getStravaStats();
+    }
+    console.log(err);
+  }
+}
+
+async function reportProgress(history, newActivities) {
+  let totalDistance = 0;
+  let totalAltitude = 0;
+  let bikeDistance = 0;
+  let bikeAltitude = 0;
+  let runDistance = 0;
+  let runAltitude = 0;
+  let participants = {};
+
+  let todayDate = new Date();
+  let todayMonth = todayDate.getMonth();
+  for (let day in history) {
+    if (history.hasOwnProperty(day)) {
+      history[day].forEach(record => {
+        if (record.skip) {
+          return;
+        }
+
+        let date = new Date(day.replace(/(\d{4})-(\d{2})-(\d{2})/, "$2/$3/$1"))
+        if (date.getMonth() === todayMonth) {
+          const name = `${record.firstname} ${record.lastname}`;
+          let participant = participants[name];
+          if (!participant) {
+            participants[name] = participant = {
+              name: name,
+              bikeDistance: 0,
+              bikeAltitude: 0,
+              runDistance: 0,
+              runAltitude: 0,
+              hikeAltitude: 0,
+              moveTime: 0
+            }
+          }
+
+          if (record.type === 'Ride' || record.type === 'VirtualRide') {
+            participant.bikeDistance += record.distance;
+            bikeDistance += record.distance;
+            participant.bikeAltitude += record.total_elevation_gain;
+            bikeAltitude += record.total_elevation_gain;
+          } else if (record.type === 'Run') {
+            participant.runDistance += record.distance;
+            runDistance += record.distance;
+            participant.runAltitude += record.total_elevation_gain;
+            runAltitude += record.total_elevation_gain;
+          } else if (record.type == 'Hike') {
+            participant.hikeAltitude += record.total_elevation_gain;
+          }
+          participant.moveTime += record.moving_time;
+        }
+        totalDistance += record.distance;
+        totalAltitude += record.total_elevation_gain;
+      });
+    }
+  }
+
+  let message = '';
+  let threadMessage = '';
+
+  // Longest activity tracker
+  //   newActivities.forEach(activity => {
+  //     let isGreaterThanAllDates = false;
+  //     for (let day in history) {
+  //       if (history.hasOwnProperty(day)) {
+  //         isGreaterThanAllDates = history[day].filter(record => {
+  //           return activity.athlete.firstname === record.firstname &&
+  //               activity.athlete.lastname === record.lastname &&
+  //               activity.type === record.type;
+  //         }).every(record => {
+  //             return activity.distance >= record.distance;
+  //         });
+  //         if (!isGreaterThanAllDates) {
+  //            break;
+  //         }
+  //       }
+  //     }
+  //     if (isGreaterThanAllDates) {
+  //       message += `Congratulations ${activity.athlete.firstname} on your longest ${activity.type}, ${Math.round(activity.distance/1000)} kilometers!
+    
+  // `;
+  //     }
+  //   });
+
+  const buildMessage = (filter, orderer, header, buildParticipantLine) => {
+    let messagePart = '';
+    const contest = Object.keys(participants).map(k => participants[k]).filter(filter).sort((a, b) => { return orderer(b) - orderer(a);});
+    if (contest.length > 0) {
+      messagePart += `${header}
+`;
+    contest.forEach(p => {
+      messagePart += `${buildParticipantLine(p)}
+`;
+    });
+    messagePart += `
+`;
+    }
+    return messagePart;
+  }
+
+  threadMessage += `Mike and Daryl Septeverist Challenge
+
+`;
+  const sumElevation = p => p.bikeAltitude + p.runAltitude + p.hikeAltitude;
+  const buildElevationParticipantLine = p => `${p.name} ${Math.round(sumElevation(p)/100)/10} km`;
+  threadMessage += buildMessage(p => sumElevation(p) >= 8.848*1000,
+    sumElevation, ':trophy: Summitteers', buildElevationParticipantLine
+  );
+  threadMessage += buildMessage(p => sumElevation(p) >= 8.44*1000 && sumElevation(p) < 8.848*1000,
+    sumElevation, 'In the Death Zone', buildElevationParticipantLine
+  );
+  threadMessage += buildMessage(p => sumElevation(p) >= 7.47*1000 && sumElevation(p) < 8.44*1000,
+    sumElevation, 'Camp 3', buildElevationParticipantLine
+  );
+  threadMessage += buildMessage(p => sumElevation(p) >= 6.4*1000 && sumElevation(p) < 7.47*1000,
+    sumElevation, 'Camp 2', buildElevationParticipantLine
+  );
+  threadMessage += buildMessage(p => sumElevation(p) >= 6.065*1000 && sumElevation(p) < 6.4*1000,
+    sumElevation, 'Camp 1', buildElevationParticipantLine
+  );
+  threadMessage += buildMessage(p => sumElevation(p) >= 5.6*1000 && sumElevation(p) < 6.065*1000,
+    sumElevation, 'Basecamp', buildElevationParticipantLine
+  );
+  threadMessage += buildMessage(p => sumElevation(p) >= 0.1*1000 && sumElevation(p) < 5.6*1000,
+    sumElevation, 'Summitting', buildElevationParticipantLine
+  );
+
+  const buildMoveTimeParticipantMessage = p => `${p.name} ${Math.round((p.moveTime)/60/6)/10} hours`;
+  message += buildMessage(p => p.moveTime > 0 && p.moveTime < 20 * 60 * 60,
+    p => p.moveTime,
+    'Keep that heart rate up. September Exercise Time Challenge',
+    buildMoveTimeParticipantMessage
+  );
+  message += buildMessage(p => p.moveTime >= 20 * 60 * 60,
+    p => p.moveTime,
+    ':trophy: Congrats to the 20 hour club!',
+    buildMoveTimeParticipantMessage
+  );
+
+  message += `This month ${Object.keys(participants).length} Sonatypers have
+  biked ${Math.round(bikeDistance/1000)} kilometers
+  biked up ${Math.round(bikeAltitude/1000)} kilometers
+  ran ${Math.round(runDistance/1000)} kilometers
+  ran up ${Math.round(runAltitude/1000)} kilometers
+
+This year Sonatypers have traveled ${Math.round(totalDistance/1000)} kilometers or ${Math.round(totalDistance/10/40075)}% around the earth!
+This year Sonatypers have climbed ${Math.round(totalAltitude/1000)} kilometers or ${Math.round(totalAltitude/8848)} times up Mount Everest!`
+
+  // const slackBot = new slack(auth.slack.token);
+  // await slackBot.conversations.join({
+  //   token: auth.slack.token,
+  //   channel: 'CCACNN4N8'
+  // });
+  // const messagesResponse = await slackBot.chat.postMessage({
+  //   token: auth.slack.token,
+  //   channel: 'CCACNN4N8',
+  //   text: message
+  // });
+  // if (threadMessage) {
+  //   await slackBot.chat.postMessage({
+  //     token: auth.slack.token,
+  //     channel: 'CCACNN4N8',
+  //     thread_ts: messagesResponse.ts,
+  //     text: threadMessage
+  //   });
+  // }
+  console.log(message);
+  console.log(threadMessage);
+}
+
+async function main() {
+  await setup();
+  await refreshStravaToken();
+  await doWork();
+}
+
+main();
